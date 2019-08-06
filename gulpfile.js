e89bee8c0690bbc301d51b9b6c8d475c9f9920ef
futuregit <gmail.com>
2019-08-05 23:02:01 -0400

    Added postcss packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
 
 gulp.task('default', () => {
   console.log("Hooray - you created a gulp task!");
@@ -9,8 +13,17 @@ gulp.task('html', () => {
   console.log("Imagine something useful being done to your HTML");
 });
 
+gulp.task('styles', () => {
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([autoprefixer, cssvars, nested]))
+    .pipe(gulp.dest('./app/temp/styles'));
+});
+
 gulp.task('watch', () => {
   watch('./app/index.html', () => {
     gulp.start('html');
+  });
+  watch('./app/assets/styles/**/*.css', () => {
+    gulp.start('styles');
   });
 });
