b0341449cb49e4948cb2dc7b4eb566fd2de8a4a2
futuregit <gmail.com>
2019-10-12 15:37:03 -0400

    Added error handling for styles task

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -8,5 +8,9 @@ cssImport =     require('postcss-import');
 gulp.task('styles', () => {
   return gulp.src('./app/assets/styles/styles.css')
     .pipe(postcss([cssImport, autoprefixer, cssvars, nested]))
+    .on('error', function(errorInfo) {
+      console.log(errorInfo.toString());
+      this.emit('end');
+    })
     .pipe(gulp.dest('./app/temp/styles'));
 });
